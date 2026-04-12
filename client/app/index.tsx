350c89ddb8103e5e289e857bc36e4ea26be1a4ea
cyklan <dev@cyklan.de>
2026-04-12 22:00:49 +0200

    add appwrite sdk to project

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { ActivityIndicator, Button, Text, View } from "react-native";
+import client from "../lib/appwrite";
+import { Locale } from "react-native-appwrite"
+
+const locale = new Locale(client);
 
 export default function Index() {
+  const [status, setStatus] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  async function ping() {
+    setLoading(true);
+    setStatus(null);
+    try {
+      // const res = await client.ping();
+      const res = await locale.get();
+      
+      setStatus(`Pong: ${res.ip}`);
+    } catch (e: any) {
+      setStatus(`Error: ${e?.message ?? String(e)}`);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", gap: 16 }}>
+      <Button title="Ping Appwrite" onPress={ping} disabled={loading} />
+      {loading && <ActivityIndicator />}
+      {status && <Text>{status}</Text>}
     </View>
   );
 }
