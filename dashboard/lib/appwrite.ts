371e0c7873904e9d38c0d4f4ce32c1a056a9b0a7
cyklan <dev@cyklan.de>
2026-04-12 22:46:20 +0200

    remove next.js project because everything will be in the same app

--- dashboard/lib/appwrite.ts
+++
@@ -1,9 +0,0 @@
-import { Account, Client } from "appwrite";
-
-const client = new Client()
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!.trim())
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!.trim());
-
-const account = new Account(client);
-
-export { account, client };
